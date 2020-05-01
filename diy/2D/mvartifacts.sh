3c2f772e5964db0b663f3cf4ef43a75a4165d412
Vijay Mahadevan <vijay.m@gmail.com>
2020-04-30 21:07:18 -0400

    Add some helper routines and links

---
+++ diy/2D/mvartifacts.sh
@@ -0,0 +1,7 @@
+CASENAME=$1
+VISDIR=/Users/mahadevan/Code/peterka/visual
+mkdir -p ${VISDIR}/${CASENAME}/
+mv *.png ${VISDIR}/${CASENAME}/
+mv *.vts ${VISDIR}/${CASENAME}/
+mv *.pvts ${VISDIR}/${CASENAME}/
+
